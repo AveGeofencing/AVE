47f58f5f43f011b64648a23bd94c492377f8dd63
Adedara <courageadedara@gmail.com>
2024-11-22 11:06:41 +0100

    Added authentication and background tasks for email sending

--- app/routers/GeneralUserRouter.py
+++ app/routers/GeneralUserRouter.py
@@ -2,16 +2,19 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.utils.APIKeys import get_api_key
+
 from ..services import UserService
 from ..database.database import get_db_session
 from ..schemas.UserSchema import UserCreateModel
 
 GeneralUserRouter = APIRouter(prefix="/user", tags=["General User"])
 DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
+api_key_dependency = Annotated[str, Depends(get_api_key)]
 
 
 @GeneralUserRouter.post("/create_user")
-async def create_new_user(user: UserCreateModel, session: DBSessionDep):
+async def create_new_user(user: UserCreateModel, session: DBSessionDep, _: api_key_dependency):
     user_service = UserService(session)
     created_message = await user_service.create_new_user(user)
 
--- app/routers/StudentRouter.py
+++ app/routers/StudentRouter.py
@@ -4,10 +4,9 @@ from fastapi import APIRouter, HTTPException, Request
 from app.auth.sessions.SessionHandler import SessionHandler
 from ..services import UserService
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
+from fastapi import Depends, BackgroundTasks
 from ..database import get_db_session
 from ..dependencies.sessionDependencies import authenticate_admin_user, authenticate_student_user
-
 StudentRouter = APIRouter(
     prefix = "/user/student",
     tags=["Users/Student"]
@@ -15,5 +14,18 @@ StudentRouter = APIRouter(
 
 DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
 authenticate_student = Annotated[dict, Depends(authenticate_student_user)]
+authenticate_admin = Annotated[dict, Depends(authenticate_admin_user)]
+
+@StudentRouter.post("/forgot_password")
+async def forgot_password(session: DBSessionDep, student_email: str, background_tasks: BackgroundTasks):
+    userService = UserService(session)
+    await userService.send_reset_password_email(user_email = student_email, background_tasks = background_tasks)
+    return {"message": "Password reset email has been sent successfully"}
 
 
+@StudentRouter.post("/reset_password")
+async def reset_password(new_password, token:str, session: DBSessionDep, background_tasks: BackgroundTasks):
+
+    userService= UserService(session)
+    message = await userService.change_password(new_password, token, background_tasks)
+    return message
