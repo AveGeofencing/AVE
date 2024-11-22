83099de7e28143eaf349e38196d136a54c58359e
Adedara <courageadedara@gmail.com>
2024-11-22 11:08:16 +0100

    Utility for email settings, and api key storage

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,13 +1,29 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import EmailStr
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     echo_sql: bool = False
+    API_KEYS: str
+    
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+class EmailSettings(BaseSettings):
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool
+    VALIDATE_CERTS: bool
+
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
+email_settings = EmailSettings()
 settings = Settings()
